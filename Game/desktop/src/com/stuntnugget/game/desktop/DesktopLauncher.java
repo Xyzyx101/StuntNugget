06a2f66d16cdb6a344272c7fb1407af4c550e3f2
Andrew Perrault <andrew.perrault@yahoo.ca>
2015-05-15 11:21:44 -0400

    Initial project commit.  Just default LibGDX hello world.

---
+++ Game/desktop/src/com/stuntnugget/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.stuntnugget.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.stuntnugget.game.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
